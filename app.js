8c4af900ba9b313807d8eefd7f357720fff284f2
Arka Nayan <itsarkanayan@gmail.com>
2016-05-22 13:27:07 +0530

    Allow cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(cors);
 
 app.use('/', routes);
 app.use('/users', users);
